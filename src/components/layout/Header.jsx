e686cd1ad4fe069f576fdc8dc13180e34e43ecda
Ying Lu <yinglu91@gmail.com>
2020-03-10 00:10:00 -0400

    add module style

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,23 +1,54 @@
 import React from 'react'
 import { Link } from 'gatsby'
+// import './Header.module.scss'
+import headerStyles from './Header.module.scss'
 
 const Header = () => {
+  console.log(headerStyles)
   return (
-    <header>
-      <h1>Bootcamp</h1>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title} to="/">
+          Bootcamp
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/"
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/blog"
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/about"
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/contact"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import '../../styles/index.scss'
+import layoutStyles from './Layout.module.scss'
 
 const Layout = props => {
   return (
-    <div>
-      <Header />
-      {props.children}
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        {props.children}
+      </div>
+
       <Footer />
     </div>
   )
